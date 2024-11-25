beffff317cce159289a299fb00db0af715a4894c
gracefeng05 <gracefeng@ucsb.edu>
2024-11-25 14:27:26 -0800

    Update employer name

--- src/Experiences.js
+++ src/Experiences.js
@@ -3,7 +3,7 @@ import "./Experiences.css";
 import { FaClock } from "react-icons/fa";
 
 const experiences = [
-  { company: "UCSB College of Engineering", role: "Teaching Assistant", location: "Santa Barbara, CA", time: "July 2024 - Present" },
+  { company: "CMPSC 156: Advanced App Programming", role: "Teaching Assistant", location: "Santa Barbara, CA", time: "July 2024 - Present" },
   { company: "Wildfell VR", role: "VR Game Creative Team", location: "Santa Barbara, CA", time: "June 2023 - Present" },
   { company: "West Monroe", role: "Software Engineer Intern", location: "San Francisco, CA", time: "June 2024 - August 2024" },
   { company: "Shellphish Cybersecurity Lab", role: "Research Apprentice", location: "Santa Barbara, CA", time: "September 2023 - June 2024" }
